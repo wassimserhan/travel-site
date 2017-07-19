97dd14daad730a0bc3842557254c24ad656c7c9f
Wassim <wserhan@mac.com>
2017-07-19 16:02:39 -0500

    Completed the modal ovelay.

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -1,15 +1,11 @@
 import MobileMenu from './modules/MobileMenu';
 import RevealOnScroll from './modules/RevealOnScroll';
 import $ from 'jquery';
-<<<<<<< HEAD
-=======
 import StickyHeader from './modules/StickyHeader';
->>>>>>> sticky-header
+import Modal from './modules/Modal';
 
 var mobileMenu = new MobileMenu();
 new RevealOnScroll($(".feature-item"), "85%");
 new RevealOnScroll($(".testimonial"), "60%");
-<<<<<<< HEAD
-=======
-var stickyHeader = new StickyHeader();  
->>>>>>> sticky-header
+var stickyHeader = new StickyHeader();
+var modal = new Modal();
